6528298447deff67b3d5d4a71dfaae190792f587
elhose <elhose>
2022-11-25 14:51:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo serves purpose as Hibernate brush up and Deep Dive
---
+++ src/main/java/com/js/hiernate/tutorial/HibernateBrushUpApplication.java
@@ -0,0 +1,13 @@
+package com.js.hiernate.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateBrushUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateBrushUpApplication.class, args);
+	}
+
+}
